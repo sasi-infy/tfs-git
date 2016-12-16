7609413ccea923e261ed6ec939130bece18f8cc4
Deepak Raja Murugan <deepakraja.murugan@infosys.com>
2016-12-16 03:55:36 +0000

    Default ASP Project
    git-tfs-id: [http://chnmct399659d:8085/tfs/DefaultCollection]$/FairyTail/GoOnline;C31

---
+++ GoOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoOnline.Startup))]
+namespace GoOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
